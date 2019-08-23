d570a03868c0219f040a9df7532b5de01375973e
yptheangel <choowilson93@gmail.com>
2019-08-23 18:14:53 +0800

    fix AsyncTask class to avoid memory leaks

--- app/src/main/java/com/yptheangel/dl4jandroid/yolo_objdetection/ObjDetection.java
+++ app/src/main/java/com/yptheangel/dl4jandroid/yolo_objdetection/ObjDetection.java
@@ -28,6 +28,7 @@ import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.util.ModelSerializer;
 import org.deeplearning4j.zoo.ZooModel;
 import org.deeplearning4j.zoo.model.TinyYOLO;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 
 import java.io.IOException;
@@ -35,18 +36,18 @@ import java.util.ArrayList;
 
 public class ObjDetection extends Activity implements CvCameraPreview.CvCameraViewListener {
 //    private CascadeClassifier faceDetector;
-    private int absoluteFaceSize = 0;
+//    private int absoluteFaceSize = 0;
     private CvCameraPreview cameraView;
 
     private static final int gridWidth = 13;
     private static final int gridHeight = 13;
     private static double detectionThreshold = 0.5;
-    private static final int tinyyolowidth = 416;
-    private static final int tinyyoloheight = 416;
-
-    String LOG_TAG="DEMO_ObjDetection";
-
-    ComputationGraph initializedModel =null;
+//    private static final int tinyyolowidth = 416;
+//    private static final int tinyyoloheight = 416;
+//
+//    String LOG_TAG="DEMO_ObjDetection";
+//
+//    ComputationGraph initializedModel =null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,12 +58,12 @@ public class ObjDetection extends Activity implements CvCameraPreview.CvCameraVi
         cameraView = findViewById(R.id.camera_view);
         cameraView.setCvCameraViewListener(this);
 
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-
-//            ZooModel model = TinyYOLO.builder().numClasses(0).build();
-
+//        new AsyncTask<Void, Void, Void>() {
+//            @Override
+//            protected Void doInBackground(Void... voids) {
+//
+////            ZooModel model = TinyYOLO.builder().numClasses(0).build();
+//
 //            try {
 ////                initializedModel = (ComputationGraph) model.initPretrained();
 //                initializedModel =ModelSerializer.restoreComputationGraph(getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/tiny-yolo-voc_dl4j_inference.v2.zip");
@@ -72,30 +73,35 @@ public class ObjDetection extends Activity implements CvCameraPreview.CvCameraVi
 //                e.printStackTrace();
 //            }
 //            if (initializedModel != null) {
+//                Log.i(LOG_TAG,"Model is successfully loaded!");
+//
 //                Log.i(LOG_TAG,initializedModel.summary());
-//                Log.i(LOG_TAG,"Yeeha!");
 //            }
 //            NativeImageLoader loader = new NativeImageLoader(tinyyolowidth, tinyyoloheight, 3, new ColorConversionTransform(COLOR_BGR2RGB));
 //            ImagePreProcessingScaler scaler = new ImagePreProcessingScaler(0, 1);
-
-                try {
-                    ArrayList<String> labels = new VOCLabelsAndroid().getLabels();
-
-                    Log.i(LOG_TAG,"Labels loaded!");
-                    Log.i(LOG_TAG,labels.toString());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                return null;
-            }
-        }.execute();
+//
+//                try {
+//                    ArrayList<String> labels = new VOCLabelsAndroid().getLabels();
+//
+//                    Log.i(LOG_TAG,"Labels is successfully loaded!");
+//                    Log.i(LOG_TAG,labels.toString());
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                return null;
+//            }
+//        }.execute();
+        AsyncTaskLoader dependencies_loader = new AsyncTaskLoader();
+        dependencies_loader.execute();
     }
+
+
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        absoluteFaceSize = (int) (width * 0.32f);
+//        absoluteFaceSize = (int) (width * 0.32f);
     }
 
     @Override
@@ -128,4 +134,48 @@ public class ObjDetection extends Activity implements CvCameraPreview.CvCameraVi
 
         return rgbaMat;
     }
+
+    private class AsyncTaskLoader extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            int tinyyolowidth = 416;
+            int tinyyoloheight = 416;
+            String LOG_TAG="DEMO_ObjDetection";
+            ComputationGraph initializedModel =null;
+
+            try {
+                initializedModel =ModelSerializer.restoreComputationGraph(getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/tiny-yolo-voc_dl4j_inference.v2.zip");
+                Log.i(LOG_TAG,initializedModel.summary());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (initializedModel != null) {
+                Log.i(LOG_TAG,"Model is successfully loaded!");
+
+                Log.i(LOG_TAG,initializedModel.summary());
+            }
+            NativeImageLoader loader = new NativeImageLoader(tinyyolowidth, tinyyoloheight, 3, new ColorConversionTransform(COLOR_BGR2RGB));
+            ImagePreProcessingScaler scaler = new ImagePreProcessingScaler(0, 1);
+
+            try {
+                ArrayList<String> labels = new VOCLabelsAndroid().getLabels();
+                Log.i(LOG_TAG,"Labels is successfully loaded!");
+                Log.i(LOG_TAG,labels.toString());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+    }
 }
+
